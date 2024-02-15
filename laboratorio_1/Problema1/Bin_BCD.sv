b31955ad31625bf33b13d6a7acade002315636f4
EmilioTec10 <110855627+EmilioTec10@users.noreply.github.com>
2024-02-15 11:33:39 -0600

    Primera parte del problema 1

--- laboratorio_1/Problema1/Bin_BCD.sv
+++ laboratorio_1/Problema1/Bin_BCD.sv
@@ -1,16 +1,18 @@
-module Bin_BCD (
+module Bin_BCD(
    input [3:0] binary_input,
    output reg [7:0] bcd_output
 );
 
-   always_comb begin
-      bcd_output = 8'b0000_0000;
+integer i;
 
-      if (binary_input >= 5) bcd_output[3:0] = binary_input + 3;
-      else bcd_output[3:0] = binary_input;
-
-      if (binary_input >= 10) bcd_output[7:4] = binary_input + 3;
-      else bcd_output[7:4] = binary_input;
+always @(binary_input) begin
+   bcd_output = 0;         
+   for (i = 0; i < 4; i = i + 1) begin             // Itera por cada bit de entrada
+      if (bcd_output[3:0] >= 5) bcd_output[3:0] = bcd_output[3:0] + 3;  // If BCD digit is >= 5, + 3
+      if (bcd_output[7:4] >= 5) bcd_output[7:4] = bcd_output[7:4] + 3;
+      bcd_output = {bcd_output[6:0], binary_input[3-i]};                  // Shift un bit, y shift por cada bit del input 
    end
+end
 
 endmodule
+
