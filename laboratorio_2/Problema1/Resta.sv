220d5e6dd23071e9bc78d62b0addaba4ce2aacc6
EmilioTec10 <110855627+EmilioTec10@users.noreply.github.com>
2024-03-04 14:48:50 -0600

    Se arreglo el modulo de la suma y la resta

--- laboratorio_2/Problema1/Resta.sv
+++ laboratorio_2/Problema1/Resta.sv
@@ -3,6 +3,16 @@
 //////////////////////  -- RESTA --  //////////////////////
 ///////////////////////////////////////////////////////////
 
+module complement #(parameter N=4)( //Modulo para obtener el complemento de un numero
+	input [N-1:0] in, //Numero
+	output reg [N-1:0] complement); //Complemento
+	
+	always_comb begin
+		complement = (~in) + 1'b1; //Operacion para obtener el complemento
+	end
+	
+endmodule
+
 module RESTA_op #(parameter N=4)(input logic [N-1:0] a, 
 											input logic [N-1:0] b,
 											output reg [N-1:0] result,
@@ -11,46 +21,37 @@ module RESTA_op #(parameter N=4)(input logic [N-1:0] a,
 											output reg flagV,
 											output reg flagZ);
 	
-	reg [N-1:0] comp2B; //Complemento a 2 de B
-	reg [N-1:0] tempRes;
+  wire [N-1:0] A_complement; //Complemento para la entrada A
+  wire [N-1:0] B_complement; //Complemento para la entrada B
+  wire [N-1:0] Sum; //Resultado de la suma 
 	
-	complement2 comp2(.in(b), .resComp2(comp2B));										
+  complement #(N) comp_A (.in(A), .complement(A_complement)); //Instancia para el complemento de A
+  complement #(N) comp_B (.in(B), .complement(B_complement)); //Instancia para el complemento de B
+
+  //Instancia para sumar el complemento del numero menor con el numero mayor
+  SUMA_op #(N) adder (.a((a >= b) ? a : A_complement), .b((a >= b) ? B_complement : b), .result(Sum), .flagC(flagC));
+	
+  //Resultado
+  assign result = Sum;										
 	
 	always_comb begin
 		
-		//INICIALIZACIÓN DE VARIABLES
-		tempRes = 0;
-		result = 0;
-		flagC = 0;
+		//Initialize variables
 		flagN = 0;
-		flagV = 0;
 		flagZ = 0;
+		flagV = 0;
 		
-		// RESTA GENERA UN NÚMERO NEGATIVO
-		if (a<b) begin
-			tempRes = a + comp2B;
-			result = (~tempRes) + 1'b1;
-			//PENSAR EL CARRY NO SE USA SIEMPRE
-			flagC = 1;
-			flagN = 1;
-			flagZ = 0;
+		if (a < b) begin //Revisa si la primera entrada es menor a la segunda
+			flagN = 1; //En ese caso el resultado es negativo
 		end
 		
-		// RESTA GENERA UN NÚMERO POSITIVO
-		else if (a>b) begin
-			result = a + comp2B;
-			flagC = 0;
-			flagN = 0;
-			flagZ = 0;
-		end
-		
-		// RESTA GENERA UN CERO
 		else if (a == b) begin
-			result = a + comp2B;
-			flagC = 0;
-			flagN = 0;
 			flagZ = 1;
-		end 
+		end
+		
+		else begin 
+			flagN = 0;
+		end
 		
 		// FLAG OVERFLOW
 		flagV = ((a[N-1] & b[N-1] & (~result[N-1])) | ((~a[N-1]) & (~b[N-1]) & result[N-1]));
