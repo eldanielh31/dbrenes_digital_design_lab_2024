6aa5f12cb8c156526f5e964f96c63953e4b67402
Daniel Brenes Gómez <52304903+eldanielh31@users.noreply.github.com>
2024-03-06 05:55:57 -0600

    cambiando multiplicacion para que no use el operador *

--- laboratorio_2/Problema1/Multiplicacion.sv
+++ laboratorio_2/Problema1/Multiplicacion.sv
@@ -3,32 +3,36 @@
 //////////////////////  -- MULTIPLICACION --  ///////////////////////
 ///////////////////////////////////////////////////////////
 
-module MULTIPLICACION_op #(parameter N=4) (input logic [N-1:0] a, 
-											input logic [N-1:0] b,
-											output reg [N-1:0] result,
-											output reg flagC,
-											output reg flagV,
-											output reg flagZ);
+module MULTIPLICACION_op #(parameter N=4) (
+    input logic [N-1:0] a,
+    input logic [N-1:0] b,
+    output reg [N-1:0] result,
+    output reg flagC,
+    output reg flagV,
+    output reg flagZ
+);
 
-	always_comb begin
-		
-		//INICIALIZACIÓN DE VARIABLES
-		flagC = 0;
-		flagV = 0;
-		flagZ = 0;
-		
-		{flagC, result} = a * b;
-		
-		// FLAG OVERFLOW
-		flagV = ((a[N-1] & b[N-1] & (~result[N-1])) | ((~a[N-1]) & (~b[N-1]) & result[N-1]));
-		
-		// FLAG CERO
-		flagZ = (result == 0);
-		
-		// Bandera de negativo
-//      flagN = (result[N-1] == 1);
-		
-	
-	end
-	
+    always_comb begin
+        // Initialize variables
+        flagC = 0;
+        flagV = 0;
+        flagZ = 0;
+        
+        // Custom multiplication algorithm
+        result = 0;
+        for (int i = 0; i < N; i = i + 1) begin
+            if (b[i] == 1'b1) begin
+                result = result + (a << i);
+            end
+        end
+        
+        // Overflow flag
+        flagV = ((a[N-1] & b[N-1] & (~result[N-1])) | ((~a[N-1]) & (~b[N-1]) & result[N-1]));
+        
+        // Zero flag
+        flagZ = (result == 0);
+        
+        // Negative flag (if needed)
+        // flagN = (result[N-1] == 1);
+    end
 endmodule
