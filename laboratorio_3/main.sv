05d7e957232d6ac51a2cc390d361fe7d048e9c4b
EmilioTec10 <110855627+EmilioTec10@users.noreply.github.com>
2024-04-10 15:03:16 -0600

    se agrego un modulo main ya para hacer merge

---
+++ laboratorio_3/main.sv
@@ -0,0 +1,17 @@
+module main(input  clock,
+				input  reset,
+				input logic left,
+				input logic right,
+				output [7:0] red,
+				output [7:0] green,
+				output [7:0] blue,
+				output vgaclock,
+				output hsync,
+				output vsync,
+				output n_blank);
+				
+				
+
+
+
+endmodule 
